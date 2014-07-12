506e28f2f24d5e64fbc170b3c09f69226d95c7ba
Akhil Mallavarapu <akhil1710@gmail.com>
2014-07-12 04:52:15 -0700

    adding access headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  after_filter :set_access_control_headers
+end
+
+def set_access_control_headers 
+    headers['Access-Control-Allow-Origin'] = 'http://10.16.20.229:8000'
+    headers['Access-Control-Request-Method'] = '*' 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,48 +17,50 @@ class PagesController < ApplicationController
 
     def search
         if request.post?
-            pollid = [params[:pollid]]
-            tag_name = [params[:tag]]
-
-            tag = Tag.find(:tag_name = tag_name)
-            if tag
+            pollid = params[:pollid]
+            tag_name = params[:tag]
+=begin
+            tag = Tag.where(tag_name: tag_name)
+            if tag.nil?
                 respond_to do |format|
                     format.json { render :json => {:status => 'false'}.to_json }
                 end
                 return
             end
+=end
             tag = Tag.new
             tag.tag_name = tag_name
-            tag.save
+            tag.save!
 
-            poll = Poll.find(:id = pollid)
+            poll = Poll.find(pollid)
             poll.tags << tag
-            poll.save
+            poll.save!
 
             client = Yelp::Client.new
             
-            include Yelp::V2::Search::Request 
+            #include Yelp::V2::Search::Request 
 
-            request = location.new(
+            request = Yelp::V2::Search::Request::Location.new(
                         :term => tag.tag_name,
-                        :address => poll.location,
+                        :address => 'San Francisco',
                         :sort => 2,
                         :limit => 1)
 
             response = client.search(request)
+            puts JSON.parse(response.to_json)['businesses']
             biz_info = JSON.parse(response.to_json)['businesses'][0]
 
             restaurant = Restaurant.new
             restaurant.name = biz_info['name']
-            restaurant.image = biz_info['photo_url']
+            restaurant.image = biz_info['image_url']
             restaurant.rating = biz_info['avg_rating']
             restaurant.address = biz_info['address']
             restaurant.city = biz_info['city']
             restaurant.state = biz_info['state']
-            restaurant.category = biz_info['category']['category_filter']
+            restaurant.category = biz_info['categories'][0][0]
             restaurant.save
             
-            html = '<li class="table-view-cell media"><a class="navigate-right" href="restaurant.html" data-transition="slide-in"><span class="badge">#</span><img class="media-object pull-left" src="restaurant_image"><div class="media-body">restaurant_name<p>restaurant_category</p></div></a></li>'
+            html = '<li class="table-view-cell media"><a class="navigate-right" href="restaurant.html" data-transition="slide-in"><span class="badge">#</span><img height="50" width="50" class="media-object pull-left" src="restaurant_image"><div class="media-body">restaurant_name<p>restaurant_category</p></div></a></li>'
 
             html.sub! 'restaurant_image', restaurant.image
             html.sub! 'restaurant_name', restaurant.name
