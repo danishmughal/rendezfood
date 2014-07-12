f6fe4565bdea30c4bfc00de2a4c7977d1708d3a4
Danish Mughal <mughal.danish@gmail.com>
2014-07-11 23:23:56 -0700

    views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,18 @@ class PagesController < ApplicationController
 
 
 	def home
+		if !user_signed_in?
+			redirect_to '/splash'
+		end
+	end
+
+	def index
+	end
+
+	def splash
+	end
+
+	def restaurant
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   root to: 'pages#home'
+  get '/splash' => 'pages#splash'
+  get '/restaurant' => 'pages#restaurant'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
